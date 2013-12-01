c66faf0ad09aa3fd35d4b41de3d12f9a9120acff
fabiovitalba <fabvit9@gmail.com>
2013-12-01 14:03:59 +0100

    V0.0.1 HW1
    -Added Gems

---
+++ Assets/Scripts/Effects/collectibleFloat.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collectibleFloat : MonoBehaviour {
+
+    public float rotationSpeed;
+    public GameObject collectiblePrefab;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float amtToRotate = rotationSpeed * Time.deltaTime;
+
+        transform.Rotate(new Vector3(0, 0, amtToRotate));
+	}
+
+    void OnTriggerEnter(Collider otherObject)
+    {
+        //if (otherObject.tag == "Lerpz")
+        {
+            Debug.Log("This Object collided: " + otherObject.tag);
+            Destroy(gameObject);
+        }
+    }
+}
